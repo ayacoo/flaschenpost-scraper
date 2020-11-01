8dab7e1bef0badd6267abbaf03d0a51a6c5676b3
Guido Schmechel <guido@ayacoo.de>
2020-11-01 15:44:59 +0100

    [TASK] Beverage categories added

---
+++ Classes/Service/Category.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Ayacoo\Flaschenpost\Service;
+
+class Category
+{
+    protected array $categories = [
+        'water' => [1, 2, 3, 4, 5, 51, 89],
+        'beer' => [6, 10, 7, 12, 13, 15, 9, 47, 83, 8, 190, 191, 88],
+        'lemonade' => [18, 19, 20, 21, 22, 46, 52, 84, 85, 24],
+        'juice' => [96, 97, 98, 99, 100, 101, 102, 103, 126, 105, 104, 106, 107]
+    ];
+}
